c3eaa3df56723e03a602c84c5b32d7c64ab69f4c
Tyler Beck <tbeck202@gmail.com>
2019-07-19 20:02:15 -0700

    Push basic Drills via desktop
    Hello, World! + Accounting Welcome page drill

---
+++ AccountingWelcomeDrill/AccountingWelcomeDrill/AccountingWelcomeDrill/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AccountingWelcomeDrill
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're counting on you!");
+            Console.ReadLine();
+        }
+    }
+}
