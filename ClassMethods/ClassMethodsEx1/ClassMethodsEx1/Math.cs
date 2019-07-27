02c0d921bef7e1019b957a2d8bf2b46bc33b3c35
Tyler Beck <tbeck202@gmail.com>
2019-07-27 14:22:37 -0700

    Commit ClassMethods drill

---
+++ ClassMethods/ClassMethodsEx1/ClassMethodsEx1/Math.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodsEx1
+{
+    public class Math
+    {
+        public static decimal DoMath (int num1)
+        {
+            decimal num1Convert = Convert.ToDecimal(num1);
+            decimal result = num1Convert / 2;
+            return result;
+        }
+
+        public static decimal DoMath (int num1, int num3)
+        {
+            decimal num1Convert = Convert.ToDecimal(num1);
+            decimal num2Convert = Convert.ToDecimal(num3);
+            decimal result = (num1Convert + num2Convert) / 2;
+            return result;
+        }
+
+        public static int GetTwentyFive (out int x)
+        {
+            int y = 5 * 5;
+            x = y;
+            return x;
+        }
+    }
+}
---
+++ ClassMethods/ClassMethodsEx1/ClassMethodsEx1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodsEx1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math math = new Math();
+            Console.WriteLine("Please enter a number");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            decimal afterMath = Math.DoMath(num1);
+            Console.WriteLine("Your number divided by two = " + afterMath);
+
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+
+            int num2 = Math.GetTwentyFive(out num2);
+            Console.WriteLine("We will now multiply your number by " + num2);
+            Console.WriteLine("Press enter to continue...");
+            Console.ReadLine();
+
+            decimal numsCombined = MoreMath.DoMoreMath(afterMath, num2);
+            Console.WriteLine("Your modified number is now: " + numsCombined);
+            Console.ReadLine();
+
+            Console.WriteLine("Please enter another number");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            decimal afterMath2 = Math.DoMath(num1, num3);
+            Console.WriteLine("Your numbers added and divided by two = " + afterMath2);
+            Console.ReadLine();
+        }
+    }
+
+    static class MoreMath
+    {
+        public static decimal DoMoreMath(decimal afterMath, int num2)
+        {
+            decimal afterMoreMath = (afterMath * num2);
+            return afterMoreMath;
+        }
+    }
+}
