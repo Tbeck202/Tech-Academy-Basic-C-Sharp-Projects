21c095b6956b978deb21c43590a63a899163fcb0
Tyler Beck <tbeck202@gmail.com>
2019-07-26 19:50:44 -0700

    Commit class and methods drill 2

---
+++ ClassAndMethods2/ClassAndMethods2/Methods.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods2
+{
+    class Methods
+    {
+        public static void doStuff(int num1, int num2)
+        {
+            int result = num1 * 36;
+            Console.WriteLine("Your first number multiplied by 36 = " + result);
+            Console.WriteLine("Your second number was: " + num2);
+        }
+    }
+}
---
+++ ClassAndMethods2/ClassAndMethods2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods myMethod = new Methods();
+            Console.WriteLine("Please enter a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter a second number.");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Methods.doStuff(num1, num2);
+            Console.ReadLine();
+        }
+    }
+}
