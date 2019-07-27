467d7ab9555db2da23109c699292f51a1f47bb60
Tyler Beck <tbeck202@gmail.com>
2019-07-26 18:40:25 -0700

    commit class and method drill

---
+++ ClassAndMethods/ClassAndMethods/Methods.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods
+{
+    class Methods
+    {
+        public static int AddOne (int userNum)
+        {
+            int userAddOne = userNum + 1;
+            return userAddOne;
+        }
+
+        public static int SubtractOne (int userNum)
+        {
+            int userSubOne = userNum - 1;
+            return userSubOne;
+        }
+
+        public  static int MultiplyTwo (int userNum)
+        {
+            int userMultTwo = userNum * 2;
+            return userMultTwo;
+        }
+    }
+}
---
+++ ClassAndMethods/ClassAndMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            int addResult = Methods.AddOne(userNum);
+            Console.WriteLine("Your number, plus one = " + addResult);
+
+            int subResult = Methods.SubtractOne(userNum);
+            Console.WriteLine("Your number, minus one = " + subResult);
+
+            int multResult = Methods.MultiplyTwo(userNum);
+            Console.WriteLine("Your number, multiplied by two = " + multResult);
+            Console.ReadLine();
+
+        }
+    }
+}
