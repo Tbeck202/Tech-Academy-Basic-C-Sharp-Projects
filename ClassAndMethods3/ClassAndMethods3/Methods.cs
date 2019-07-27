481e5f79d3d2d1c9831c0131d4f6580e8d694cad
Tyler Beck <tbeck202@gmail.com>
2019-07-26 21:06:58 -0700

    Commit Class and Methods drill 3

---
+++ ClassAndMethods3/ClassAndMethods3/Methods.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods3
+{
+    class Methods
+    {
+        public static void DoStuff(int num1)
+        {
+            int result = num1 + 5;
+            Console.WriteLine("12 + 5 = " + result);
+        }
+
+        public static decimal DoStuff(decimal num2)
+        {
+            decimal result = num2 / 2;
+            return result;
+        }
+
+        public static string DoStuff(string num3)
+        {
+            int numConvert = Convert.ToInt32(num3);
+            int result = (numConvert * 7) - 2;
+            string numToString = Convert.ToString(result);
+            return numToString;
+        }
+
+    }
+}
---
+++ ClassAndMethods3/ClassAndMethods3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndMethods3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Methods myMethods = new Methods();
+
+            //DRILL 1------------------------------------------------------------------
+            int num1 = 12;
+            Methods.DoStuff(num1);
+            Console.WriteLine("press enter to continue");
+            Console.ReadLine();
+
+            // DRILL 2 -------------------------------------------------------------------------
+            decimal num2 = 12.5m;
+            decimal returnValue2 = Methods.DoStuff(num2);
+            int decToInt = Convert.ToInt32(returnValue2);
+            Console.WriteLine("12.5 divided by 2 = " + returnValue2 + " (Which rounds down to: " + decToInt + ")");
+            Console.WriteLine("press enter to continue");
+            Console.ReadLine();
+
+            //  DRILL 3-------------------------------------------------------------------------
+            Console.WriteLine("Enter a number.");
+            string num3 = Console.ReadLine();
+            string returnValue = Methods.DoStuff(num3);
+
+            Console.WriteLine("We did stuff to your number and now it is: " + returnValue);
+            Console.WriteLine("press enter to exit program");
+            Console.ReadLine();
+        }
+    }
+}
