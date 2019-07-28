c21c0c16fd86dc14e7046d205c283d5a150efa7e
Tyler Beck <tbeck202@gmail.com>
2019-07-28 16:37:35 -0700

    Commit OperatorOverloading drill

---
+++ OperatorOverloading/OperatorOverloading/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+
+            employee1.Id = 1;
+            employee2.Id = 2;
+
+            bool idCheck = (employee1 == employee2);
+            if (idCheck == true)
+                Console.WriteLine("Employees cannot have matching ID's!");
+            else
+                Console.WriteLine("Everything is okay");
+
+            Console.ReadLine();
+        }
+    }
+}
