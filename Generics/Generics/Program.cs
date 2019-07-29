88daef80831de90841db624a7550767453b9f1db
Tyler Beck <tbeck202@gmail.com>
2019-07-28 18:21:01 -0700

    Commit Generics drill

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Tyler", "Joe", "Jill" };
+            Employee<string> employees = new Employee<string>();
+            {
+                employees.Things = names.ToList();
+            }
+
+            List<int> idNum = new List<int>() { 1, 2, 3 };
+            Employee<int> id = new Employee<int>();
+            {
+                id.Things = idNum.ToList();
+            }
+
+            foreach (string employee in names)
+            {
+                Console.WriteLine(employee);
+            }
+            foreach (int num in idNum)
+            {
+                Console.WriteLine(num);
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
