49c751af70a4a3291cf59fe1d59cc4fdee943025
Tyler Beck <tbeck202@gmail.com>
2019-07-28 19:47:39 -0700

    Commit Enum drill

---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What day of the week is it?");
+            string currentDay = Console.ReadLine();
+
+            try
+            {
+                DaysOfWeek day = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), currentDay);
+                Console.WriteLine("Today is: " + day);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week");
+            }
+            Console.ReadLine();
+        }
+
+        public enum DaysOfWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+    }
+}
