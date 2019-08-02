722b543e75c0da4a5f233a327c60641744edbad2
Tyler Beck <tbeck202@gmail.com>
2019-08-01 21:15:21 -0700

    Commit DateTime Drill
    This drill is not complete. Still working on it.  I just need to quit for the night.

---
+++ DateTimeDrill/DateTimeDrill/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine("The current Date and time = {0}", now);
+
+            Console.WriteLine("Enter a number.");
+            DateTime num = Convert.ToDateTime(Console.ReadLine());
+
+            TimeSpan hourModify = now + hourModify;
+
+            Console.WriteLine("{0}", hourModify);
+            Console.ReadLine();
+        }
+    }
+}
