6a3ca84f2474866dc75927ced7f17579611a7748
Tyler Beck <tbeck202@gmail.com>
2019-08-01 20:28:59 -0700

    Commit system.IO drill

---
+++ SystemIO_Drill/SystemIO_Drill/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SystemIO_Drill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Yo! Gimme a number!");
+            string num = Convert.ToString(Console.ReadLine());
+
+            File.WriteAllText(@"C:\Users\Tyler\Logs\logdrill.txt", num);
+
+            string read = File.ReadAllText(@"C:\Users\Tyler\Logs\logdrill.txt");
+            Console.WriteLine(read);
+            Console.ReadLine();
+        }
+    }
+}
