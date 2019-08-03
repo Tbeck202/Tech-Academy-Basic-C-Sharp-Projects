d873d6e1c1da18449c1e31f99ee61095f9fe14d1
Tyler Beck <tbeck202@gmail.com>
2019-08-02 19:38:16 -0700

    ConstConstructorChainDrill

---
+++ ConstConstructorChainDrill/ConstConstructorChainDrill/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstConstructorChainDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string schoolName = "The Tech Academy";
+            Console.WriteLine("Welcome to {0}! Please tell us your name.", schoolName);
+            var studentName = Console.ReadLine();
+            Random newId = new Random();
+            int id = newId.Next(1, 100);
+            Student student = new Student(studentName, id);
+            Console.WriteLine("Thanks {0}! Your ID number is: {1}", studentName, id);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConstConstructorChainDrill/ConstConstructorChainDrill/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstConstructorChainDrill
+{
+    public class Student
+    {
+        public Student(int id) : this("Name unknown", id)
+        {
+        }
+        public Student(string name, int id)
+        {
+            Name = name;
+            Id = id;
+        }
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+}
