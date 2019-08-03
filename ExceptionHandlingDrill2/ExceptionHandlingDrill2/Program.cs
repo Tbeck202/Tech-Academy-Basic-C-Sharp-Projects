c139adc113058850957df698c8b9addc876d4633
Tyler Beck <tbeck202@gmail.com>
2019-08-02 22:09:19 -0700

    Commit ExceptionHandlingDrill2

---
+++ ExceptionHandlingDrill2/ExceptionHandlingDrill2/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandlingDrill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine("Hello. How old are you?");
+            try
+            {
+                int userAge = Convert.ToInt32(Console.ReadLine());
+
+                if (userAge < 0)
+                {
+                    Console.WriteLine("Please enter a valid whole number, greater than zero.");
+                    Console.ReadLine();
+                    return;
+                }
+                int yearBorn = now.Year - userAge;
+                Console.WriteLine("You were born in " + yearBorn + "!");
+                Console.ReadLine();
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Text input not allowed. \nPlease enter a valid whole number, greater than zero.");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Error.");
+                Console.ReadLine();
+            }
+            
+            //bool validAnswer = false;
+            //int userAge = 0;
+            //while (!validAnswer)
+            //{
+            //    Console.WriteLine("Hello. How old are you?");
+            //    validAnswer = int.TryParse(Console.ReadLine(), out userAge);
+            //}
+            //if (userAge < 0)
+            //{
+            //    Console.WriteLine("Please enter a valid whole number, greater than zero.");
+            //    Console.ReadLine();
+            //    return;
+            //}
+            //else
+            //{
+
+            //    int yearBorn = now.Year - userAge;
+            //    Console.WriteLine("You were born in " + yearBorn+ "!");
+            //    Console.ReadLine();
+            //}
+        }
+    }
+}
